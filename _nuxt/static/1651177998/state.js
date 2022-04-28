e56b3cc3daf28ef496c6db571c412d179a852cf9
Bilal Punjabi <bilalthepunjabi@gmail.com>
2022-04-28 20:38:40 +0000

    0fcef4e

---
+++ _nuxt/static/1651177998/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbilalthepunjabi.com\u002F_nuxt\u002Fstatic\u002F1651177998",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fbilalthepunjabi.com\u002F",assetsPath:"\u002Fbilalthepunjabi.com\u002F_nuxt\u002F",cdnURL:a}}}}(null));
